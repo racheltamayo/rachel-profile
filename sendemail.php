87b7ef71ee2b28dd8b655b1fa7298a3405bf0e94
racheltamayo <75687043+racheltamayo@users.noreply.github.com>
2022-10-17 19:19:35 -0300

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -1,45 +1,43 @@
 <?php
-
-
-require 'phpmailer/PHPMailer.php';
-require 'phpmailer/SMTP.php';
-require 'phpmailer/Exception.php';
-
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 
-$mail = new PHPMailer();
+require_once 'phpmailer/Exception.php';
+require_once 'phpmailer/PHPMailer.php';
+require_once 'phpmailer/SMTP.php';
 
-$mail->isSMTP();
+$mail = new PHPMailer(true);
 
-$mail->Host = 'smtp.gmail.com';
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = "tls";
-$mail->Port = '587';
+$alert = '';
 
-$mail->Username = 'ermitzy@gmail.com'; // Gmail address which you want to use as SMTP server
-$mail->Password = 'cjstivdvohrcrpwl'; // Gmail address Password
+if(isset($_POST['submit'])){
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
 
+  try{
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'ermitzy@gmail.com'; // Gmail address which you want to use as SMTP server
+    $mail->Password = 'cjstivdvohrcrpwl'; // Gmail address Password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = '587';
 
-$mail->Subject = 'Message Received (Contact Page)';
+    $mail->setFrom('ermitzy@gmail.com'); // Gmail address which you used as SMTP server
+    $mail->addAddress('ermitzy@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
 
-$mail->setFrom('ermitzy@gmail.com');
+    $mail->isHTML(true);
+    $mail->Subject = 'Message Received (Contact Page)';
+    $mail->Body = "<h3>Name : $name <br>Email: $email <br>Message : $message</h3>";
 
-$mail->isHTML(true);
-$mail->Body = "<h3>Name : $name <br>Email: $email <br>Message : $message</h3>";
-
-
-$mail->addAddress('ermitzy@gmail.com');
-
-
-if ($mail->Send()){
-
-	echo"Email Sent..!";
-}else{
-
-	echo "Error..!";
+    $mail->send();
+    $alert = '<div class="alert-success">
+                 <span>Message Sent! Thank you for contacting us.</span>
+                </div>';
+  } catch (Exception $e){
+    $alert = '<div class="alert-error">
+                <span>'.$e->getMessage().'</span>
+              </div>';
+  }
 }
-
-
-$mail->smtpClose();
+?>
